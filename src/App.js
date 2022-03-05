1652ec0fd0deb32e2b3d519134c62d4fee1d88b0
Cfy1010 <cy_one2000@yahoo.fr>
2022-03-05 19:16:31 +0100

    add Navbar from Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div>
-     <h1>Hello React</h1>
+      <Navbar />
+      <h1>Hello React</h1>
     </div>
   );
 }
